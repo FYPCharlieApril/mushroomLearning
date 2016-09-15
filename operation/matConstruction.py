e419ffc3fdd50b80f75caa3f809439a760206788
Charlie Chen <Charliechen_1@163.com>
2016-09-15 13:31:59 +0800

    Added data source.

---
+++ operation/matConstruction.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def constructH(dMat, fList, hMat, cols):
+    #TODO
+    for dSlide in dMat:
+        thisList = [0] * cols
+        for fea in dSlide:
+            index = np.argwhere(fList == fea)
+    return
+
+def constructDv(dMat):
+    #TODO
+    return
+
+def constructW(dMat):
+    #TODO
+    return
--- src/main.py
+++ src/main.py
@@ -1,31 +1,28 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+
+import operation.matConstruction as mc
 
 data_src = '../dataFile/agaricus-lepiota.data'
-
-def constructH(data):
-    #TODO
-    return
-
-def constructDv(data):
-    #TODO
-    return
-
-def constructW(data):
-    #TODO
-    return
-
-
+testNum = 20
 
 #data_src = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
 df = pd.read_csv(data_src, header=None)
 
 # df is the data of the mushrooms, size 8123 * 23, with 1 colume of label and 22 for feature, 8123 data slots
 df.tail()
-
 dMat = df.iloc[:].values
+
 # definition of V and E
 # compute the column number of E matrix
-cols = sum (list(map(lambda xs : len(np.unique(xs)), zip(*dMat)))[1:])
-print(cols)
+fList = list(enumerate(map(lambda xs : list(set(xs)), zip(*dMat))))
+fList = list(map(list, fList))
+fList = np.concatenate(list(map(lambda xs : list(map(lambda x : x + str(xs[0]),xs[1])), fList))[1:])
+print (fList)
+
+
+
+
+
+
+
