3d13d02efba76a643cfd4ea28e4dbd12d79c7de4
Charlie Chen <Charliechen_1@163.com>
2016-09-15 11:54:48 +0800

    Added data source.

--- src/main.py
+++ src/main.py
@@ -7,16 +7,15 @@ def constructH(data):
 
 
 
-data_src = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
+#data_src = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
 data_src = '../dataFile/agaricus-lepiota.data'
 df = pd.read_csv(data_src, header=None)
 
 # df is the data of the mushrooms, size 8123 * 23, with 1 colume of label and 22 for feature, 8123 data slots
 df.tail()
 
-testcases = df.iloc[0:20].values
+dMat = df.iloc[:].values
 # definition of V and E
 # compute the column number of E matrix
-cols = sum(map(lambda xs : len(np.unique(xs)), zip(*testcases)))
-
+cols = sum (list(map(lambda xs : len(np.unique(xs)), zip(*dMat)))[1:])
 print(cols)
