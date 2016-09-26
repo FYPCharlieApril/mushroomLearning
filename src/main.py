4f0552f50347331fdf8a7495b98751fad02215c2
Charlie Chen <Charliechen_1@163.com>
2016-09-26 13:28:22 +0800

    20 data are now randomly picked

--- src/main.py
+++ src/main.py
@@ -10,18 +10,12 @@ data_src = '../dataFile/agaricus-lepiota.data'
 
 df = pd.read_csv(data_src, header=None)
 
-ranList = [random.randint(0,1000) for _ in range(testNum)]
-
 # df is the data of the mushrooms, size 8123 * 23, with 1 colume of label and 22 for feature, 8123 data slots
 df.tail()
 dMat = df.iloc[:].values
 
-# here we randomly pick testNum data for experiments
-testCases = []
-for i in range(testNum):
-    testCases.append(dMat[ranList[i]].tolist())
-testCases = np.matrix(testCases)
+X = dMat[0]
+y = dMat[1:]
 
 tv = tv.TotalVariation
-hMat = tv.constructH(testCases,dMat)
-
+tv.fit(tv, dMat, 20)
