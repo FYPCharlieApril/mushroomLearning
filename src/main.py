8dac74b05c73e5f02fd3692974db73dc9fdd8a25
Charlie Chen <Charliechen_1@163.com>
2016-11-01 15:11:53 +0800

    20 data are now randomly picked

--- src/main.py
+++ src/main.py
@@ -1,21 +1,27 @@
 import numpy as np
 import pandas as pd
 import random
-import src.TotalVariation as tv
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
-testNum = 20
+def encodeData(df, catFeaList):
+    le = LabelEncoder()
+    X = df.values
+    ohe = OneHotEncoder(categorical_features=catFeaList)
+    for i in range(X.shape[1]):
+        X[:, i] = le.fit_transform(X[:, i])
+    H = ohe.fit_transform(X).toarray()
+    return H
 
+# read the data
 data_src = '../dataFile/agaricus-lepiota.data'
-#data_src = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
-
-df = pd.read_csv(data_src, header=None)
-
 # df is the data of the mushrooms, size 8123 * 23, with 1 colume of label and 22 for feature, 8123 data slots
-df.tail()
-dMat = df.iloc[:].values
+df = pd.read_csv(data_src)
+# data_src = 'https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data'
 
-X = dMat[0]
-y = dMat[1:]
+H = encodeData(df, range(df.shape[1]))
+print(H)
 
-tv = tv.TotalVariation
-tv.fit(tv, dMat, 20)
+
+
+
