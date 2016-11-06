db95afa35bdec08a2feb8b6efb4c7d2a2a8e67aa
Charlie Chen <Charliechen_1@163.com>
2016-11-06 22:27:02 +0800

    20 data are now randomly picked

--- src/main.py
+++ src/main.py
@@ -16,5 +16,32 @@ df.columns = Header
 h = hg.hyper_graph(weight=None, head=None, tail=None, df=df, \
                    catFeaList=range(22), label_mapping={'e': 1, 'p': -1})
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+train_size_arr = [20,40,60,80,100,120,140,160,180,200]
+result = []
+for train_size_ent in train_size_arr:
+    X,y = h.hMat, h.y
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, train_size=train_size_ent, random_state=0)
+    sc = StandardScaler()
+    sc.fit(X_train)
+    y_train = list(y_train)
+    from sklearn.linear_model import Perceptron
+
+    ppn = Perceptron(n_iter=40, eta0=0.1, random_state=1)
+    ppn.fit(X_train, y_train)
+
+    y_pred = ppn.predict(X_test)
+    result.append((y_test != y_pred).sum()/y_test.shape[0])
 
 
+import matplotlib.pyplot as plt
+plt.plot(train_size_arr, result, marker='o')
+plt.ylim([0, 0.15])
+plt.ylabel('Error rate')
+plt.xlabel('Number of test data')
+plt.grid()
+plt.tight_layout()
+plt.show()
--- src/subgradient_method.py
+++ src/subgradient_method.py
@@ -1,3 +1,6 @@
 class subgradient_method:
     def __init__(self):
         print("TO BE DONE")
+
+    def markov_operator(self):
+        print("TO BE DONE")
