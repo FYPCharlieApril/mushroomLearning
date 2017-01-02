f534ff0c0b86c2981d7cd0d0b422e192a63c4490
Charlie Chen <Charliechen_1@163.com>
2017-01-02 20:52:14 +0800

    qu ta ma de commit msg

--- src/total_variance_method.py
+++ src/total_variance_method.py
@@ -1,3 +1,4 @@
+import numpy as np
 def __init__(self, sigma, seta, tau):
     self.seta = seta
     self.sigma = sigma
@@ -7,9 +8,19 @@ def fit_predict(self, X, y):
     pass
 
 
-def proximal(self, X, y, f):
-    pass
-
+def proximal(self, x_, y, gf, s1=None):
+    if gf == "square_norm":
+        prox = 1/(1+self.tau) * (x_+self.tau * y)
+    if gf == "indicator":
+        if s1 is None:
+            print("Need to input s1 for this method")
+            return
+        else:
+            v = 1/(1+self.tau) * (x_+self.tau * y)
+            dino = np.norm(v)
+            dino = max([dino, 1])
+            prox = (x_ + self.tau * s1) / dino
+    return prox
 
 def lovazs_extension(self, set_func):
     pass
