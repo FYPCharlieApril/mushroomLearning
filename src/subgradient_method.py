5da761f3964feac232c0a9005e203ab25734d0fd
Charlie Chen <Charliechen_1@163.com>
2017-01-08 17:45:59 +0800

    PDHG for w 2 finished

--- src/subgradient_method.py
+++ src/subgradient_method.py
@@ -15,6 +15,7 @@ class subgradient_method:
             if i not in y_train_ind:
                 self.y_un_ind.append(i)
         self.y_un_ind = np.array(self.y_un_ind)
+        self.y = y
 
     def construct_h_mat(self, X, y, y_train_ind):
         hg = hyper_graph(weight=np.array([1] * X.shape[0]),
@@ -31,7 +32,6 @@ class subgradient_method:
     def markov_operator(self,  f):
         # here we compute A and W
         v_size, e_size = self.hg.hMat.shape[0], self.hg.hMat.shape[1]
-        f_index = enumerate(f)
         W = np.zeros((v_size, v_size))
         A = np.zeros((v_size, v_size))
         head, tail = self.hg.head, self.hg.tail
@@ -46,14 +46,13 @@ class subgradient_method:
                 v = v_can[0]
                 A[u, v] = A[u, v] + self.hg.weight[e]
                 A[v, u] = A[u, v]
+
         for u in self.y_un_ind:
             W[u, u] = sum(A[u, :])
-        print(W.sum)
-
         # following are teh procedures of computing the Markov operator, here the projection matrix we use
         # are the one with all entries to be 1.
         f_out = (W-A).dot(f)
-        f_out[self.y_train_ind] = f[self.y_train_ind]
+        f_out[self.y_train_ind] = self.y[self.y_train_ind]
         return f_out
 
     def sgm(self,f):
@@ -62,13 +61,14 @@ class subgradient_method:
         #f_iter = self.markov_operator(f_iter)
 
         #while(abs(LA.norm(f_iter - f_last)) < 100):
-        while (t < 400):
-           gn = self.markov_operator(f_iter)
-           f_last = f_iter
-           f_iter = f_iter - (0.9/LA.norm(gn)) * gn
-           f_iter[self.y_train_ind] = f[self.y_train_ind]
-           t += 1
-        self.end_time = time.time()
+        while (t < 200):
+            print("Step:", t+1, "Current accuracy:", np.array(np.where(f_iter == self.y)).shape[1] / self.y.shape[0])
+            gn = self.markov_operator(f_iter)
+            f_last = f_iter
+            f_iter = f_iter - (0.9/LA.norm(gn)) * gn
+            f_iter[self.y_train_ind] = self.y[self.y_train_ind]
+            t += 1
+            self.end_time = time.time()
         return f_iter
 
 
@@ -86,11 +86,9 @@ class subgradient_method:
         f_n = self.sgm(f_n)
 
         f_avg = 0.5 * (f_p + f_n)
-        U = np.where((f_index[:, 1] != 1) & (f_index[:, 1] != -1))[0]
-        threshold = sum(f_avg[U])/len(U)
-        for i in range (len(f_avg)):
+        threshold = sum(f_avg[self.y_un_ind])/len(self.y_un_ind)
+        for i in self.y_un_ind:
             f_avg[i] = 1 if f_avg[i] > threshold else -1
-        f_avg[self.y_train_ind] = f[self.y_train_ind]
         return f_avg
 
 
