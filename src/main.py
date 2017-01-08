63e11e3bf4659ffd49048e489ed14887bf26df75
Charlie Chen <Charliechen_1@163.com>
2017-01-08 10:56:13 +0800

    PDHG for w 2 finished

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ df['label'] = df['label'].map({'e': 1, 'p': -1})
 X, y = df.values[:, 1:] , df.values[:, 0]
 
 y_ind = np.matrix(list(enumerate(y)))
-X_train, X_test, y_train, y_test = train_test_split(X, y_ind, train_size=20)
+X_train, X_test, y_train, y_test = train_test_split(X, y_ind, train_size=40)
 ind_test, y_test = y_test[:, 0], y_test[:, 1]
 ind_train, y_train = y_train[:, 0], y_train[:, 1]
 
